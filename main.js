672417335e88ee1c44afe897067ae7343c84a73b
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-10-22 00:08:43 -0400

    updates

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-    $(".list-view").hide();
-})
-
-$(".viewBtn-list").click(function() {
-    $(".grid-view").hide();
-    $(".list-view").show();
+$(document).ready(function () {
+  $(".list-view").hide();
 });
 
-$(".viewBtn-grid").click(function() {
-    $(".list-view").hide();
-    $(".grid-view").show();
+$(".viewBtn-list").click(function () {
+  $(".grid-view").hide();
+  $(".list-view").show();
+  $(this).css("opacity", "1");
+  $(".viewBtn-grid").css("opacity", ".5");
+});
+
+$(".viewBtn-grid").click(function () {
+  $(".list-view").hide();
+  $(".grid-view").show();
+  $(this).css("opacity", "1");
+  $(".viewBtn-list").css("opacity", ".5");
 });
